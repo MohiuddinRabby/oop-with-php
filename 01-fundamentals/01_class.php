2caf66a2751eb31ab3bb6af8d9456c6403c274e7
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-09-29 14:01:00 +0600

    basic class example

--- 01-fundamentals/01_class.php
+++ 01-fundamentals/01_class.php
@@ -1,2 +1,37 @@
 <?php
-echo 'helo';
+class Human
+{
+    //property
+    public $name;
+    //method
+    public function sayHello()
+    {
+        echo "hi {$this->name} \n";
+    }
+}
+class Cat
+{
+    //method
+    public function sayHello()
+    {
+        echo "mewoo\n";
+    }
+}
+class Dog
+{
+    //method
+    public function sayHello()
+    {
+        echo "woff!\n";
+    }
+}
+//Human object
+$humanOne = new Human();
+$humanOne->name = "Doe"; //set
+$humanOne->sayHello();
+//Cat object
+$catOne = new Cat();
+$catOne->sayHello();
+//Dog object
+$dogOne = new Dog();
+$dogOne->sayHello();
